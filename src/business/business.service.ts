ab89630a3f90716d9fef62cdd1d4a526e4b606e1
Sajawal Jutt <mail.sajawaljutt@gmail.com>
2025-10-30 19:11:36 +0500

    Update business service

--- src/business/business.service.ts
+++ src/business/business.service.ts
@@ -9,27 +9,27 @@ import { UpdateBusinessDto } from "./update-business.dto";
 @Injectable()
 export class BusinessService {
     constructor(
-    @InjectRepository(Business) private readonly repo: Repository<Business>,
-    @InjectRepository(User) private readonly users: Repository<User>,
+    @InjectRepository(Business) private readonly businessrepository: Repository<Business>, 
+    @InjectRepository(User) private readonly usersRepository: Repository<User>,
     ) {}
 
     async createBusiness(dto: CreateBusinessDto, ownerId: string) {
-        const owner = await this.users.findOneBy({ id: ownerId });
-        const business = this.repo.create({ dto, owner });
-        return this.repo.save(business);
+        const owner = await this.usersRepository.findOneBy({ id: ownerId });
+        const business = this.businessrepository.create({ dto, owner });
+        return this.businessrepository.save(business); 
     }
     async updateBusiness(id: string, dto: UpdateBusinessDto, ownerId: string){
-        const business = await this.repo.findOne({ where: { id }, relations: { owner: true } });
+        const business = await this.businessrepository.findOne({ where: { id }, relations: { owner: true } });
         if (!business || business.owner.id !== ownerId) throw new NotFoundException('Business not found');
         Object.assign(business, dto);
-        return this.repo.save(business);
+        return this.businessrepository.save(business);
     }
     async findMine(ownerId: string) {
-    return this.repo.find({ where: { owner: { id: ownerId } } });
+    return this.usersRepository.find({ where: { owner: { id: ownerId } } });
   }
 
   async findOne(id: string) {
-  const business = await this.repo.findOne({
+  const business = await this.businessrepository.findOne({
     where: { id },
   });
   if (!business) {
