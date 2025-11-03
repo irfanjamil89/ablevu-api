a2a0f4c06c81c0b1b147dadfdf9c546b18cd8761
Farhan-SE <farhanaslam9582@gmail.com>
2025-11-03 17:53:10 +0500

    ye

--- src/business/accessibility.dto.ts
+++
@@ -1,7 +0,0 @@
-import { ArrayMinSize, IsArray } from 'class-validator';
-export class AccessibilityFeatureDto {
-    @IsArray()
-    @ArrayMinSize(1)
-    AccessibilityFeatureID: string[];
-
-}
--- src/business/update-business.dto.ts
+++
@@ -1,4 +0,0 @@
-import { CreateBusinessDto } from './create-business.dto';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class UpdateBusinessDto extends PartialType(CreateBusinessDto) {}
