ad658d859e65b8beaf194d4c58f408277c0890e6
Farhan-SE <farhanaslam9582@gmail.com>
2025-12-09 18:38:51 +0500

    Added name find in business questions

--- src/business questions/business-question.module.ts
+++ src/business questions/business-question.module.ts
@@ -4,8 +4,9 @@ import { BusinessQuestions } from 'src/entity/business-questions.entity';
 import { BusinessQuestionsService } from './business-question.service';
 import {BusinessQuestionsController} from './business-questions.controller'
 import { Business } from 'src/entity/business.entity';
+import { User } from 'src/entity/user.entity';
 @Module({
-  imports: [TypeOrmModule.forFeature([BusinessQuestions, Business])],
+  imports: [TypeOrmModule.forFeature([BusinessQuestions, Business, User])],
   providers: [BusinessQuestionsService],
   controllers: [BusinessQuestionsController],
   exports: [BusinessQuestionsService]
