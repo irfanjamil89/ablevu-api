e1b5988181dde5d095946f8d0e01b77fb2561848
irfan jamil <hirfan@wynshop.com>
2025-12-22 07:11:23 +0500

    github deploy

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,10 @@ import { WinstonModule } from 'nest-winston';
 import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
-  const httpsOptions = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem'),
-};
+//   const httpsOptions = {
+//   key: fs.readFileSync('key.pem'),
+//   cert: fs.readFileSync('cert.pem'),
+// };
 
   const app = await NestFactory.create(AppModule,{
     logger: WinstonModule.createLogger({
