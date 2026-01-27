d8626338e764958b379d4bd00be5e483e8ac8cb0
irfan jamil <hirfan@wynshop.com>
2026-01-27 20:51:30 +0500

    update s3 bucket

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -5,7 +5,7 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UsersService } from 'src/services/user.service';
 import { User } from 'src/entity/user.entity';
-import { ConfigService } from 'src/services/config.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -13,7 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env.JWT_SECRET || '',
+      secretOrKey: configService.get('JWT_SECRET').toString()
     });
   }
 
