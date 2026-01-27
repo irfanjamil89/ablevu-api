35adcb60e725cbca675d7190751afd6ef27fc675
irfan jamil <hirfan@wynshop.com>
2026-01-27 21:23:22 +0500

    update s3 bucket

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,13 +6,18 @@ import { LocalStrategy } from './local.strategy';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [AuthService, LocalStrategy,JwtStrategy, ConfigService],
-  imports: [UserModule, PassportModule,JwtModule.register({
-      secret: process.env.Jwt_Secret || '' ,
-      signOptions: { expiresIn: '3600s' },
+  imports: [UserModule, PassportModule,JwtModule.registerAsync({
+     imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (config: ConfigService) => {
+    const secret = config.get<string>('JWT_SECRET');
+    if (!secret) throw new Error('JWT_SECRET missing');
+    return { secret, signOptions: { expiresIn: '1d' } };
+  }
     })],
   controllers: [AuthController],
 })
